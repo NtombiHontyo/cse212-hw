d265639b4d030732e7fbe7abd0edfdf30a132268
NtombiHontyo <nhontyo@byupathway.edu>
2025-06-14 20:23:52 +0200

    Assignment 1-3 done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,10 +1,13 @@
+using System.Diagnostics;
+
 public class Node
 {
+        
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
 
-    public Node(int data)
+    public Node (int data)
     {
         this.Data = data;
     }
@@ -12,7 +15,10 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
+        if (value == Data)
+        {
+            return;
+        }
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +40,71 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+         if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            // Insert to the left
+            if (Left is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else
+        {
+            // Insert to the right
+            if (Right is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
+        
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int highest_tree = 1;
+        if (Left is null && Right is null)
+        {
+            int node_count = 1;
+            return node_count;
+        }
+
+        else
+        {
+            if (Data > Left.Data)
+            {
+                int height = 1 + Left.GetHeight();
+                if (height > highest_tree)
+                {
+                    highest_tree = height;
+                }
+                return highest_tree;
+            }
+            else 
+            {
+                int height = 1 + Right.GetHeight();
+                if (height > highest_tree)
+                {
+                    highest_tree = height;
+                }
+                return highest_tree;
+            }
+            
+        }
+            
+        // Replace this line with the correct return statement(s)
+
     }
 }
